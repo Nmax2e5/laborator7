188a7d28c187acb10fa73c78ce93560ca39f76ba
Nmax2e5 <74609995+Nmax2e5@users.noreply.github.com>
2020-12-10 14:00:14 +0200

    program1final

--- program1.c
+++ program1.c
@@ -6,25 +6,23 @@ int main()
 {
     FILE* in  = fopen("input.txt",  "r");
 
-    char *c=(char*) malloc(1001 * sizeof(char));
-    int len=0;
+    char *c=(char*) malloc(1001);
     char *ans=NULL;
 
     while(fscanf(in, "%[^\n]\n", c)==1)
     {
         if(ans==NULL)
         {
-            ans=(char*) malloc(strlen(c)*sizeof(char));
+            ans=(char*) malloc(strlen(c));
             strcpy(ans,c);
         }
         else
         {
-            ans=(char*) realloc(ans, (len+strlen(c))*sizeof(char));
-             strcat(ans, c);
+            ans=(char*) realloc(ans, (strlen(ans)+strlen(c)));
+            strcat(ans, c);
         }
     }
 
-    //ans[len]='\0';
     printf("%s", ans);
 
     return 0;
